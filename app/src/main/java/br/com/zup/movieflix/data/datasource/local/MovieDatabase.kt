c6f0cf999e65f47e020ab6de67eb1a8f69662500
jeehmilena <jessicamilenacorrea@gmail.com>
2022-07-01 15:33:29 -0300

    inserção da camada de repository e refatoração do MovieuseCase

---
+++ app/src/main/java/br/com/zup/movieflix/data/datasource/local/MovieDatabase.kt
@@ -0,0 +1,35 @@
+package br.com.zup.movieflix.data.datasource.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import br.com.zup.movieflix.data.datasource.local.dao.MovieDAO
+import br.com.zup.movieflix.domain.model.Movie
+
+@Database(entities = [Movie::class], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getDatabase(context: Context): MovieDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java,
+                    "filme_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
