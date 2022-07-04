fe853cc01a082cf547d444a1b017fa852c4a46cc
jeehmilena <jessicamilenacorrea@gmail.com>
2022-07-04 14:29:29 -0300

    Adição do modelo com base no JSON

---
+++ app/src/main/java/br/com/zup/movieflix/data/model/MovieResponse.kt
@@ -0,0 +1,15 @@
+package br.com.zup.movieflix.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("page")
+    val page: Int = 0,
+    @SerializedName("results")
+    val results: List<Result> = listOf(),
+    @SerializedName("total_pages")
+    val totalPages: Int = 0,
+    @SerializedName("total_results")
+    val totalResults: Int = 0
+)
